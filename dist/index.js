ea8f6ebf688557cb90503f9b60e53bcc6d8d2530
Ishaan Sheikh <sheikh.ysbh@gmail.com>
2020-08-29 23:57:04 +0530

    dist updated

--- dist/index.js
+++ dist/index.js
@@ -8831,8 +8831,8 @@ async function createPost(data, secret) {
       },
     });
 
-    var data = await res.json();
-    return data;
+    console.log(res);
+    return res;
   } catch (e) {
     core.setFailed("An error has been occured " + e);
     return false;
